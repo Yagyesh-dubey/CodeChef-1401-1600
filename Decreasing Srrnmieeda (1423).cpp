925d80e96d826aefb7314c2b588b9baa308d2723
yagyesh dubey <yagyesh2002.bpl@gmail.com>
2023-04-14 01:43:17 +0530

    Create Decreasing Srrnmieeda (1423).cpp

---
+++ Decreasing Srrnmieeda (1423).cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define LL long long
+using namespace std ;
+int main(){
+
+            int tc ;cin>>tc ;
+            while(tc--){
+                 int l,r ;
+                 cin>>l>>r ;
+                 if(r>=l*2)cout<<-1<<endl ;
+                 else cout<<r<<endl ;
+
+            }
+
+
+}
